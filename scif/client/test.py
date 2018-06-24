a5393cd207cbf88461172bc9543485956fc676f5
Vanessa Sochat <vsochat@stanford.edu>
2018-06-24 13:53:21 -0700

    adding init

---
+++ scif/client/test.py
@@ -0,0 +1,45 @@
+'''
+
+Copyright (C) 2017 The Board of Trustees of the Leland Stanford Junior
+University.
+Copyright (C) 2016-2017 Vanessa Sochat.
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
+
+from scif.logger import bot
+import sys
+import pwd
+import os
+
+def main(args,parser,subparser):
+
+    from scif.main import ScifRecipe
+    cmd = args.cmd
+    client = ScifRecipe(quiet=True, writable=args.writable)
+
+    if len(cmd) == 0:
+        bot.warning('You must supply an appname to test.')
+        bot.custom(prefix="Example: ", message="scif test <app>")
+        sys.exit(1)
+
+    app = cmd.pop(0)
+
+    # Remaining arguments indicate options/args to pass on
+    if len(cmd) == 0:
+        cmd = None
+
+    client = ScifRecipe(quiet=True, writable=args.writable)
+    client.test(app, args=cmd)
