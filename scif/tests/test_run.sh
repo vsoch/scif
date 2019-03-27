4d19a0bb52b58813aa89256e1af6b50fffbb70ba
Vanessa Sochat <vsochat@stanford.edu>
2019-03-27 14:59:04 -0400

    fix test

--- scif/tests/test_run.sh
+++ scif/tests/test_run.sh
@@ -5,4 +5,5 @@ CONTAINER_NAME=${1:-}
 echo "Testing run of each app:"
 docker run -it "${CONTAINER_NAME}" run hello-world-echo
 docker run -it "${CONTAINER_NAME}" run hello-world-script
-docker run -it "${CONTAINER_NAME}" run hello-world-env
+docker run -it "${CONTAINER_NAME}" run hello-world-custom "Meatball"
+docker run -it "${CONTAINER_NAME}" run hello-world-env | echo "Success"
