a33434a5cacf50df5055d3b3b09be9a0a751d99a
Scott McMillan <smcmillan@nvidia.com>
2020-01-06 14:42:11 -0600

    alternative pattern

--- scif/main/commands.py
+++ scif/main/commands.py
@@ -171,13 +171,11 @@ def run(self, app=None, args=None):
     args: a list of one or more additional arguments to pass to runscript
 
     '''
-    # Cut out early if the app doesn't have a runscript
+    interactive = False
     config = self.app(app)
     if 'apprun' not in config:
         bot.debug('%s does not have a runscript.' % app)
         interactive = True
-    else:
-        interactive = False
 
     self.activate(app, args=args)    # checks for existence
                                      # sets _active to app's name
